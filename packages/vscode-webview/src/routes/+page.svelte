3057d6571d299e2af4c5fe4ec84446965c6a69a6
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-06 21:51:34 +0200

    switch to vanilla svelte

--- packages/vscode-webview/src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<div class="bg-red-50">
-	<h1>Welcome to SprigKit</h1>
-	<p>
-		Visit <a href="https://sprigkit.sigfredo.dev">sprigkit.sigfredo.dev</a> to read the documentation
-	</p>
-</div>
--- packages/vscode-webview/vite.config.ts
+++ packages/vscode-webview/vite.config.ts
@@ -1,12 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import { viteSingleFile } from 'vite-plugin-singlefile';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		viteSingleFile({
-			useRecommendedBuildConfig: false
-		})
-	]
+    plugins: [svelte(), viteSingleFile()],
 });
